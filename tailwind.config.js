0e4b60587ab9bf3843f1a43e5aaecc239011cafb
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-27 20:11:51 +0100

    setup: tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
   "./src/**/*.{vue,js,ts,jsx,tsx}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'light-gray':'#F4F6FA',
+        'gray-navy':'#626C7F',
+        'blueish':'#ABC1E1',
+        'navy':'#3B4D66',
+        'dark-navy':'#313E51',
+        'green':'#26D782',
+        'red':'#EE5454',
+        'purple':'#A729F5',
+      }
+    },
   },
   plugins: [],
 }
