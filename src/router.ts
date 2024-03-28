073489263b33148792f9eedd58d16b68f1551dd1
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-28 19:47:13 +0100

    setup: router

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import { createRouter, createMemoryHistory } from 'vue-router';
+import StartScreen from './views/StartScreen.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: StartScreen
+  }
+];
+const router = createRouter({
+  routes,
+  history: createMemoryHistory()
+});
+
+export default router;
