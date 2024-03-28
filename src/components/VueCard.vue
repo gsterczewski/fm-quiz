d6d8c6fdf3efdb15ff711ff4e01705f108fc9cf2
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-28 16:37:12 +0100

    feat: add VueCard.vue component

---
+++ src/components/VueCard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+type CardProps = {
+  text: string;
+};
+defineProps<CardProps>();
+</script>
+<template>
+  <div class="flex items-center gap-4 md:gap-8 p-3 bg-white rounded-md">
+    <slot></slot>
+    <p class="text-lg md:text-3xl font-medium text-blue-900">{{ text }}</p>
+  </div>
+</template>
