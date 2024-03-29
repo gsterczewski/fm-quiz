9f84057d40997962c9888827b5ceb35fb1b9c442
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-29 06:58:37 +0100

    feat: start screen draft

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    <h1>FM Quiz App</h1>
+    <header class="h-20 mb-8 border-2 border-red-500"></header>
+    <main>
+      <router-view></router-view>
+    </main>
   </div>
 </template>
-
-<style scoped>
-
-</style>
