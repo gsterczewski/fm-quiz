b7b5bf6e04d077ed6d1fcdc8232d549ffd84bc96
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-29 05:43:57 +0100

    setup: add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      },
+      {
+        find: '@components',
+        replacement: fileURLToPath(new URL('./src/components', import.meta.url))
+      },
+      {
+        find: '@views',
+        replacement: fileURLToPath(new URL('./src/views', import.meta.url))
+      },
+      {
+        find: '@images',
+        replacement: fileURLToPath(new URL('./src/assets/images', import.meta.url))
+      }
+    ]
   }
-})
+});
