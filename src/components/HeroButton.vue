bb64366a5c0acbe816a3b28c3db9c9e711d81528
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-29 17:10:53 +0100

    feat: add :focus state on HeroButton

--- src/components/HeroButton.vue
+++ src/components/HeroButton.vue
@@ -1,3 +1,11 @@
 <template>
-  <button class="w-full text-lg md:text-3xl font-medium py-4 md:py-8 rounded-md bg-purple-500 text-white hover:bg-purple-200"><slot></slot></button>
+  <button class="hero-button outline-none w-full text-lg md:text-3xl font-medium py-4 md:py-8 rounded-md bg-purple-500 text-white">
+    <slot></slot>
+  </button>
 </template>
+<style scoped>
+.hero-button:hover,
+.hero-button:focus {
+  @apply bg-purple-200;
+}
+</style>
