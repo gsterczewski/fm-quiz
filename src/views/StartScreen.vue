019425fcca0297305e84b8fe18688e89afea2caa
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-29 14:10:48 +0100

    add main landmark in StartScreen view

--- src/views/StartScreen.vue
+++ src/views/StartScreen.vue
@@ -7,28 +7,30 @@ const { quizzes } = useQuizzesStore();
 </script>
 
 <template>
-  <div class="max-w-6xl mx-auto px-6 md:px-16 flex flex-col gap-10 md:gap-16 xl:flex-row xl:gap-0 xl:px-0">
-    <div class="flex-1">
-      <h1 class="max-w-sm md:max-w-md font-light text-4xl md:text-6xl text-blue-900 mb-4">
-        Welcome to the <span class="font-bold">Frontend Quiz!</span>
-      </h1>
-      <CaptionText>Pick a subject to get started...</CaptionText>
+  <main>
+    <div class="max-w-6xl mx-auto px-6 md:px-16 flex flex-col gap-10 md:gap-16 xl:flex-row xl:gap-0 xl:px-0">
+      <div class="flex-1">
+        <h1 class="max-w-sm md:max-w-md font-light text-4xl md:text-6xl text-blue-900 mb-4">
+          Welcome to the <span class="font-bold">Frontend Quiz!</span>
+        </h1>
+        <CaptionText>Pick a subject to get started...</CaptionText>
+      </div>
+      <nav class="flex flex-col gap-3 md:gap-6 flex-1">
+        <RouterLink
+          v-for="quiz in quizzes"
+          :key="quiz.key"
+          :to="`/quizz?title=${quiz.key}`"
+          class="link border-2 border-transparent rounded-md outline-none transition-all"
+        >
+          <VueCard :text="quiz.title">
+            <VueTile :color="quiz.color">
+              <img alt="" :src="quiz.icon" class="w-10" />
+            </VueTile>
+          </VueCard>
+        </RouterLink>
+      </nav>
     </div>
-    <nav class="flex flex-col gap-3 md:gap-6 flex-1">
-      <RouterLink
-        v-for="quiz in quizzes"
-        :key="quiz.key"
-        :to="`/quizz?title=${quiz.key}`"
-        class="link border-2 border-transparent rounded-md outline-none transition-all"
-      >
-        <VueCard :text="quiz.title">
-          <VueTile :color="quiz.color">
-            <img alt="" :src="quiz.icon" class="w-10" />
-          </VueTile>
-        </VueCard>
-      </RouterLink>
-    </nav>
-  </div>
+  </main>
 </template>
 <style scoped>
 .link:hover,
