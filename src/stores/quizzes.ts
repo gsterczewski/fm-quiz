4fa6394397f10208c96ee1aa3a780ce4954e2080
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-29 06:41:22 +0100

    feat: add quizzes store and data.json

---
+++ src/stores/quizzes.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia';
+import quizzesData from '../data.json';
+
+type QuizQuestion = {
+  question: string;
+  options: string[];
+  answer: string;
+};
+type Quiz = {
+  key: string;
+  title: string;
+  icon: string;
+  color: string;
+  questions: QuizQuestion[];
+};
+
+export const useQuizzesStore = defineStore('quizes', () => {
+  const createIconPath = (icon: string) => `/${icon}`;
+  const quizzes: readonly Quiz[] = quizzesData.quizzes.map(({ title, questions, color, icon }) => ({
+    title,
+    questions,
+    icon: createIconPath(icon),
+    color,
+    key: title.toLowerCase()
+  }));
+  const getQuizz = (title: string): Quiz | undefined => quizzes.find((quizz) => quizz.title === title);
+
+  return { quizzes, getQuizz };
+});
