73805e8ff6802b76a1cf830183259734a11c5137
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-30 12:36:39 +0100

    refactor: use <slot> instead text prop

--- src/components/VueCard.vue
+++ src/components/VueCard.vue
@@ -1,12 +1,6 @@
-<script setup lang="ts">
-type CardProps = {
-  text: string;
-};
-defineProps<CardProps>();
-</script>
+<script setup lang="ts"></script>
 <template>
   <div class="flex items-center gap-4 md:gap-8 p-3 bg-white rounded-md">
     <slot></slot>
-    <p class="text-lg md:text-3xl font-medium text-blue-900">{{ text }}</p>
   </div>
 </template>
