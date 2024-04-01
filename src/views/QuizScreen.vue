eaf0a5bb71d5181df872069ddf6cafa93ddc7182
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-04-01 15:26:29 +0200

    feat: add QuizResults component

--- src/views/QuizScreen.vue
+++ src/views/QuizScreen.vue
@@ -3,10 +3,12 @@ import { useRoute } from 'vue-router';
 import { useQuizzesStore } from '@stores/quizzes';
 import useQuiz from '@/composables/useQuiz';
 
+import HeroText from '@components/HeroText.vue';
 import VueLayout from '@components/VueLayout.vue';
+import VueTitle from '@/components/VueTitle.vue';
 import CaptionText from '@components/CaptionText.vue';
-import VueTile from '@/components/VueTile.vue';
 import QuizForm from '@/components/Quiz/QuizForm.vue';
+import QuizResults from '@/components/Quiz/QuizResults.vue';
 
 const route = useRoute();
 const { getQuizz } = useQuizzesStore();
@@ -17,14 +19,21 @@ const quiz = useQuiz(quizData);
 <template>
   <VueLayout v-if="quiz">
     <template v-slot:page-title>
-      <div class="flex items-center gap-4 md:gap-6">
-        <VueTile class="w-10 aspect-square md:w-14" :color="quizData.color">
-          <img class="w-7 aspect-square md:w-10" :src="quizData.icon" />
-        </VueTile>
-        <p class="text-lg md:text-2xl text-blue-900 font-medium">{{ quizData.title }}</p>
-      </div>
+      <VueTitle :text="quizData.title" :color="quizData.color" :icon="quizData.icon" />
     </template>
-    <template v-slot:main-content>
+    <template v-slot:main-content v-if="quiz.isOver.value">
+      <HeroText text="Quiz completed" textBold="You scored..." class="flex-1" />
+      <QuizResults
+        class="flex-1"
+        :score="quiz.correctAnswersCount.value"
+        :max-score="quiz.questionsCount"
+        :color="quizData.color"
+        :icon="quizData.icon"
+        :title="quizData.title"
+      />
+    </template>
+
+    <template v-slot:main-content v-else>
       <div class="flex-1">
         <CaptionText class="mb-3 md:mb-7">{{ `Question ${quiz.questionIndex.value + 1} of ${quiz.questionsCount}` }}</CaptionText>
         <p class="xl:max-w-md text-blue-900 font-medium text-xl md:text-4xl">{{ quiz.question }}</p>
