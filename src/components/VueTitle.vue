43d2a7bebd80afca54de6b7bc0c1119bc02b7d18
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-04-01 15:23:11 +0200

    refactor: add VueTitle component to encapsulate repeated styling

---
+++ src/components/VueTitle.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import VueTile from '@components/VueTile.vue';
+type VueTileProps = {
+  text: string;
+  icon: string;
+  color: string;
+};
+defineProps<VueTileProps>();
+</script>
+<template>
+  <div class="flex items-center gap-4 md:gap-6">
+    <VueTile class="w-10 aspect-square md:w-14" :color="color">
+      <img class="w-7 aspect-square md:w-10" :src="icon" />
+    </VueTile>
+    <p class="text-lg md:text-2xl text-blue-900 font-medium">{{ text }}</p>
+  </div>
+</template>
