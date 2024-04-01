22fe0f6d2a4198351e97d6b403c28362d75f6d28
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-04-01 07:30:31 +0200

    feat: add workign quiz logic

--- src/views/QuizScreen.vue
+++ src/views/QuizScreen.vue
@@ -2,62 +2,73 @@
 import { ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { useQuizzesStore } from '@stores/quizzes';
+import useQuiz, { QUIZ_ROUND_RESULTS } from '@/composables/useQuiz';
+
+import VueLayout from '@components/VueLayout.vue';
 import CaptionText from '@components/CaptionText.vue';
-import VueCard from '@components/VueCard.vue';
-import VueTile from '@components/VueTile.vue';
 import HeroButton from '@components/HeroButton.vue';
+import QuizOption from '@components/QuizOption.vue';
+
 const ANSWER_INDEXES = ['A', 'B', 'C', 'D'];
 const route = useRoute();
 const { getQuizz } = useQuizzesStore();
 const quizKey = route.params.key;
-const quiz = getQuizz(quizKey as string);
-const currentQuestionIndex = ref(5);
-const currentQuestion = computed(() => quiz?.questions[currentQuestionIndex.value]);
-const totalQuestions = computed(() => quiz?.questions.length);
+const quiz = useQuiz(getQuizz(quizKey as string)!);
+const selectedAnswer = ref('');
+
+const answerSubmitted = computed(
+  () => quiz.result.value === QUIZ_ROUND_RESULTS.CORRECT_ANSWER || quiz.result.value === QUIZ_ROUND_RESULTS.INCORRECT_ANSWER
+);
+function handleClick() {
+  if (quiz.isOver.value) {
+    alert(`Results : ${quiz.correctAnswersCount.value}/${quiz.questionsCount}`);
+    return;
+  }
+  if (answerSubmitted.value) {
+    selectedAnswer.value = '';
+    quiz.nextQuestion();
+  } else {
+    quiz.checkAnswer(selectedAnswer.value);
+  }
+}
+
+const buttonLabel = computed(() => (quiz.isOver.value ? 'Show Results' : answerSubmitted.value ? 'Next Question' : 'Submit Answer'));
 </script>
 <template>
   <main>
-    <div v-if="quiz" class="max-w-6xl mx-auto px-6 md:px-16 flex flex-col gap-10 md:gap-16 xl:flex-row xl:gap-0 xl:px-0">
+    <VueLayout v-if="quiz">
       <div class="flex-1">
-        <CaptionText class="mb-3 md:mb-7">{{ `Question ${currentQuestionIndex + 1} of ${totalQuestions}` }}</CaptionText>
-        <p class="xl:max-w-md text-blue-900 font-medium text-xl md:text-4xl">{{ currentQuestion?.question }}</p>
+        <CaptionText class="mb-3 md:mb-7">{{ `Question ${quiz.questionIndex.value + 1} of ${quiz.questionsCount}` }}</CaptionText>
+        <p class="xl:max-w-md text-blue-900 font-medium text-xl md:text-4xl">{{ quiz.question }}</p>
       </div>
       <div class="flex-1">
         <form>
-          <label class="block custom-radio-label mb-3" v-for="(option, index) in currentQuestion?.options" :key="index" :for="`answer-${index + 1}`">
-            <input class="custom-radio" type="radio" :id="`answer-${index + 1}`" name="answer" :value="option" />
-            <VueCard class="custom-radio-card border-2" :text="option">
-              <VueTile class="custom-radio-tile">{{ ANSWER_INDEXES[index] || index + 1 }}</VueTile>
-            </VueCard>
-          </label>
-          <HeroButton type="submit" @click.prevent="">Submit Answer</HeroButton>
+          <QuizOption
+            v-for="(option, index) in quiz.possibleAnswers.value"
+            :key="index"
+            input-name="answer"
+            v-model="selectedAnswer"
+            :input-value="option"
+            :answer-index="ANSWER_INDEXES[index] || `${index + 1}`"
+            :answer="option"
+            :has-success="quiz.result.value === QUIZ_ROUND_RESULTS.CORRECT_ANSWER && selectedAnswer === option"
+            :has-error="quiz.result.value === QUIZ_ROUND_RESULTS.INCORRECT_ANSWER && selectedAnswer === option"
+            :show-success-icon="
+              quiz.result.value === QUIZ_ROUND_RESULTS.INCORRECT_ANSWER && selectedAnswer !== option && option === quiz.answer.value
+            "
+            :disabled="quiz.result.value === QUIZ_ROUND_RESULTS.CORRECT_ANSWER || quiz.result.value === QUIZ_ROUND_RESULTS.INCORRECT_ANSWER"
+          />
+
+          <HeroButton type="submit" @click.prevent="handleClick">{{ buttonLabel }}</HeroButton>
+          <p v-if="quiz.result.value === QUIZ_ROUND_RESULTS.NO_ANSWER" class="flex align-center justify-center text-red-500 mt-4 md:text-2xl">
+            <img class="inline-block w-6 md:w-8 aspect-square mr-3" src="/icon-error.svg" /> <span>Please select and answer</span>
+          </p>
         </form>
       </div>
-    </div>
+    </VueLayout>
     <div v-else>
       <h1>Sorry something went wrong</h1>
       <RouterLink to="/" class="hover:text-blue-400 hover:underline">Go back to home page</RouterLink>
     </div>
   </main>
 </template>
-<style scoped>
-.custom-radio {
-  @apply absolute opacity-0 cursor-pointer h-0 w-0;
-}
-.custom-radio-card,
-.custom-radio-tile {
-  @apply transition-all;
-}
-.custom-radio-label:hover .custom-radio-card {
-  @apply border-purple-500;
-}
-.custom-radio-label:hover .custom-radio-card > .custom-radio-tile {
-  @apply bg-purple-500 text-white;
-}
-.custom-radio:focus ~ .custom-radio-card {
-  @apply border-purple-500;
-}
-.custom-radio:focus ~ .custom-radio-card > .custom-radio-tile {
-  @apply bg-purple-500 text-white;
-}
-</style>
