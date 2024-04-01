ea9e7862f85d5a80d9c6a578b598abf221e9fec0
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-04-01 15:21:55 +0200

    feat: add useLink prop to render HeroButton as RouterLink

--- src/components/HeroButton.vue
+++ src/components/HeroButton.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+type HeroButtonProps = {
+  asLink?: boolean;
+  to?: string;
+};
+withDefaults(defineProps<HeroButtonProps>(), {
+  asLink: false,
+  to: ''
+});
+</script>
 <template>
-  <button class="hero-button outline-none w-full text-lg md:text-3xl font-medium py-4 md:py-8 rounded-md bg-purple-500 text-white">
+  <RouterLink
+    v-if="asLink"
+    :to="to"
+    class="hero-button block text-center outline-none w-full text-lg md:text-3xl font-medium py-4 md:py-8 rounded-md bg-purple-500 text-white"
+  >
+    <slot></slot>
+  </RouterLink>
+  <button v-else class="hero-button outline-none w-full text-lg md:text-3xl font-medium py-4 md:py-8 rounded-md bg-purple-500 text-white">
     <slot></slot>
   </button>
 </template>
