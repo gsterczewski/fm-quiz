d680523bb85b65cd5c8fb09e9594783171ea4832
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-04-01 15:22:50 +0200

    refactor: add HeroText component to encapsulate repeated styling

---
+++ src/components/HeroText.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+type VueTitleProps = {
+  text: string;
+  textBold: string;
+};
+defineProps<VueTitleProps>();
+</script>
+<template>
+  <h1 class="max-w-sm md:max-w-md font-light text-4xl md:text-6xl text-blue-900 mb-4">
+    {{ text }} <span class="font-bold">{{ textBold }}</span>
+  </h1>
+</template>
